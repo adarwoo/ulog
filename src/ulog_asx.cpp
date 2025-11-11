d52db87e65c87b40416d52b5d6df698eadeef96e
guillaume.arreckx <software@arreckx.com>
2025-11-11 22:41:04 +0100

    Adding C++ impl

---
+++ src/ulog_asx.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file ulog_asx.cpp
+ * @brief ASX for AVR implementation of the ULOG logging framework.
+ * @author software@arreckx.com
+ */
+#include <interrupt.h>
+
+#include <string_view>
+
+#include <asx/uart.hpp>
+#include <asx/ulog.hpp>
+#include <asx/reactor.hpp>
+
+// Allow selecting the UART to use for ULOG
+// To be added to conf_ulog.h if needed
+#ifndef ULOG_UART
+#  define ULOG_UART 0
+#endif
+
+// C Linkage for the ULOG functions called from ulog.c
+extern "C" {
+   void _ulog_transmit();
+   reactor_handle_t _ulog_asx_react_to_initiate_transmit = REACTOR_NULL_HANDLE;
+}
+
+namespace {
+   // UART type
+   using uart = asx::uart::Uart<
+      ULOG_UART,
+      asx::uart::CompileTimeConfig<
+         115200,
+         asx::uart::width::_8,
+         asx::uart::parity::none,
+         asx::uart::stop::_1
+      >
+   >;
+
+   /**
+    * Initialize the ULOG system
+    * The logging can start before this is called, but no data will be sent
+    */
+   __attribute__((constructor()))
+   void init() {
+      uart::init();
+      uart::disable_rx();
+      uart::get().CTRLA = 0; // No interrupt at this stage
+
+      // Register a reactor for filling the buffer
+      _ulog_asx_react_to_initiate_transmit = asx::reactor::bind(_ulog_transmit);
+
+      // When the uart is done sending a frame, we can send the next one
+      uart::react_on_send_complete(_ulog_asx_react_to_initiate_transmit);
+
+      // Kickstart the transmission process
+      asx::reactor::Handle(_ulog_asx_react_to_initiate_transmit).notify();
+   }
+} // End of anonymous namespace
+
+extern "C" void _ulog_asx_send_data(const uint8_t *data, size_t len) {
+   uart::send(std::span<const uint8_t>(data, len));
+}
+
+
